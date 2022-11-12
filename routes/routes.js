d7fa2cc90216898e67517e8b92751d16f00e9701
Alexander <hi.apetrov@gmail.com>
2022-11-12 11:20:27 +0000

    Routes options added

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,38 @@
 const Items = require( '../items/Items' );
 
+const itemSchema = {
+    type: 'object',
+    properties: {
+        id:{ type: 'string' },
+        name:{ type: 'string' },
+        },
+}
+
+const getItemsOpts = {
+    schema: {
+        response: {
+            200: {
+                type: 'array',
+                items: itemSchema
+                },
+            },
+        },
+    }
+const geItemOpts = {
+    schema: {
+        response: {
+            200: itemSchema,
+        }
+    }
+}
+
 function itemRoutes( fastify, options, done)  {
-    fastify.get('/items', (req, res) => {
+    //Get Items array
+    fastify.get('/items', getItemsOpts, (req, res) => {
         res.send(Items)
     })
-
-    fastify.get( '/items/:id', ( req, res ) => {     
+    //Get single item
+    fastify.get( '/items/:id', geItemOpts, ( req, res ) => {     
         const { id } = req.params
         const item = Items.find( ( item ) => item.id === id)
         res.send( item )
